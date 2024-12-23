a4d4e8666fec32c084eccb13ca370c37e8fab094
xinggang.li <xinggang.li@horizon.cc>
2024-12-23 14:54:28 +0800

    提交下，新的activity，

---
+++ Exported/launcher/src/main/java/com/horizon/main/MainActivity.java
@@ -0,0 +1,20 @@
+package com.horizon.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.UnityTechnologies.com.unity.template.urpblank.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction()
+//                    .replace(R.id.container, MainFragment.newInstance())
+//                    .commitNow();
+//        }
+    }
+}
