bf2d473353aaf3050aef826c9affadf16695ea9e
xinggang.li <xinggang.li@horizon.cc>
2024-12-23 19:55:55 +0800

    按钮，继承，等等都好了，不过默认的surfaceview应该是自动提供surface的，这个不符合要求，我们需要手动创建surface

--- Exported/launcher/src/main/java/com/horizon/main/MainActivity.kt
+++ Exported/launcher/src/main/java/com/horizon/main/MainActivity.kt
@@ -2,16 +2,28 @@ package com.horizon.main
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.FrameLayout
 import com.UnityTechnologies.com.unity.template.urpblank.R
+import com.unity3d.player.*
 
-class MainActivity : Activity() {
+class MainActivity : UnityPlayerActivity() {
+    val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fl = findViewById<FrameLayout>(R.id.fl_content);
+        fl.addView(mUnityPlayer)
         //        if (savedInstanceState == null) {
 //            getSupportFragmentManager().beginTransaction()
 //                    .replace(R.id.container, MainFragment.newInstance())
 //                    .commitNow();
 //        }
+        var btn = findViewById<Button>(R.id.button)
+        btn.setOnClickListener {
+            Log.i(TAG, "onCreate: btn clicked")
+        }
     }
 }
